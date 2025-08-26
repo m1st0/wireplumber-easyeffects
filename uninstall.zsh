2f317b54f1af0d705d561892a9f0939695990b3f
Maulik Mistry <mistry01@gmail.com>
2025-08-26 06:36:35 -0700

    Readme and comment cleanup

---
+++ uninstall.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Maulik Mistry <mistry01.com>
+# Copyright (c) 2025 Maulik Mistry
+#
+# This project is licensed under the Apache 2.0 License. See the LICENSE.txt file for details.
+#
+# If you appreciate my work or help, consider supporting me through donations.
+# You can donate via Venmo at https://venmo.com/code?user_id=3319592654995456106&created=1756212520  
+# or PayPal at https://www.paypal.com/paypalme/m1st0 
+
+# TODO: Add removal of script, restart, and folder cleanup if empty
